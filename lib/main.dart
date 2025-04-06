a635ba9e555fa2632ef56bf7859f03c382e37d3b
Katkar , Nandai <nandaikatkar29@gmail.com>
2025-04-06 14:33:05 +0530

    another final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-
 import 'package:first_app/gradient_container.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: GradientContainer(
           const Color.fromARGB(255, 202, 87, 228),
           const Color.fromARGB(255, 87, 181, 228),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
 }
